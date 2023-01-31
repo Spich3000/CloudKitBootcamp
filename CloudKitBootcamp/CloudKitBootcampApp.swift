0e356497dd61a23698529599a80c1a6c02497eeb
Spich <spichakovda@gmail.com>
2023-02-01 02:00:28 +0300

    Initial Commit

---
+++ CloudKitBootcamp/CloudKitBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudKitBootcampApp.swift
+//  CloudKitBootcamp
+//
+//  Created by Дмитрий Спичаков on 01.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct CloudKitBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
